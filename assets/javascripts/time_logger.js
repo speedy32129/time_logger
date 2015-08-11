6f10883571b486bbf42ba455b7807c95be89bca5
speedy32129 <ironspeeder@hotmail.com>
2015-08-11 14:50:50 -0400

    -- fixed javascripts time_logger.js variable declaration and added icon sets

--- assets/javascripts/time_logger.js
+++ assets/javascripts/time_logger.js
@@ -2,7 +2,7 @@
  * This script updates the element 'id' with 'newContent' if the two contents differ
  */
 function updateElementIfChanged(id, newContent) {
-    el = $(id);
+    var el = $(id);
     if (el.innerHTML != newContent) { el.update(newContent); }
 }
 
